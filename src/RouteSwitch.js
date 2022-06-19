09d9d79466a30c3fc40f6a84e016d9045a85e378
shaneSleeman <shanesleeman1@gmail.com>
2022-06-19 13:48:16 +1000

    Register click in shop

--- src/RouteSwitch.js
+++ src/RouteSwitch.js
@@ -10,28 +10,30 @@ import ShopItem from "./components/ShopItem";
 const RouteSwitch = () => {
   const [cartItems, setCartItems] = useState([]);
   const [shopItems, setShopItems] = useState([
-    <ShopItem image="https://source.unsplash.com/200x200/?tshirt" description="Shirt 1" price="30"/>,
-            <ShopItem image="https://source.unsplash.com/200x200/?shirt" description="Shirt 2" price="25"/>,
-            <ShopItem image="https://source.unsplash.com/200x200/?sleeves" description="Shirt 3" price="40"/>,
-            <ShopItem image="https://source.unsplash.com/200x200/?pants" description="Pants 1" price="32"/>,
-            <ShopItem image="https://source.unsplash.com/200x200/?trousers" description="Pants 2" price="42"/>,
-            <ShopItem image="https://source.unsplash.com/200x200/?shorts" description="Pants 3" price="25"/>,
-            <ShopItem image="https://source.unsplash.com/200x200/?shoe" description="Shoes 1" price="120"/>,
-            <ShopItem image="https://source.unsplash.com/200x200/?shoes" description="Shoes 2" price="80"/>,
-            <ShopItem image="https://source.unsplash.com/200x200/?boots" description="Shoes 3" price="70"/>,
-            <ShopItem image="https://source.unsplash.com/200x200/?cap" description="Hat 1" price="20"/>,
-            <ShopItem image="https://source.unsplash.com/200x200/?hat" description="Hat 2" price="15"/>,
-            <ShopItem image="https://source.unsplash.com/200x200/?fedora" description="Hat 3" price="40"/>,
-            <ShopItem image="https://source.unsplash.com/200x200/?jumper" description="Jacket 1" price="60"/>,
-            <ShopItem image="https://source.unsplash.com/200x200/?jacket" description="Jacket 2" price="40"/>,
-            <ShopItem image="https://source.unsplash.com/200x200/?hoodie" description="Jacket 3" price="30"/>
+    <div onClick={addCartItem}><ShopItem image="https://source.unsplash.com/200x200/?tshirt" description="Shirt 1" price="30" sid="0"/></div>,
+            <div onClick={addCartItem}><ShopItem onClick={addCartItem} image="https://source.unsplash.com/200x200/?shirt" description="Shirt 2" price="25" sid="1"/></div>,
+            <div onClick={addCartItem}><ShopItem image="https://source.unsplash.com/200x200/?sleeves" description="Shirt 3" price="40" sid="2"/></div>,
+            <div onClick={addCartItem}><ShopItem image="https://source.unsplash.com/200x200/?pants" description="Pants 1" price="32" sid="3"/></div>,
+            <div onClick={addCartItem}><ShopItem image="https://source.unsplash.com/200x200/?trousers" description="Pants 2" price="42" sid="4"/></div>,
+            <div onClick={addCartItem}><ShopItem image="https://source.unsplash.com/200x200/?shorts" description="Pants 3" price="25" sid="5"/></div>,
+            <div onClick={addCartItem}><ShopItem image="https://source.unsplash.com/200x200/?shoe" description="Shoes 1" price="120" sid="6"/></div>,
+            <div onClick={addCartItem}><ShopItem image="https://source.unsplash.com/200x200/?shoes" description="Shoes 2" price="80" sid="7"/></div>,
+            <div onClick={addCartItem}><ShopItem image="https://source.unsplash.com/200x200/?boots" description="Shoes 3" price="70" sid="8"/></div>,
+            <div onClick={addCartItem}><ShopItem image="https://source.unsplash.com/200x200/?cap" description="Hat 1" price="20" sid="9"/></div>,
+            <div onClick={addCartItem}><ShopItem image="https://source.unsplash.com/200x200/?hat" description="Hat 2" price="15" sid="10"/></div>,
+            <div onClick={addCartItem}><ShopItem image="https://source.unsplash.com/200x200/?fedora" description="Hat 3" price="40" sid="11"/></div>,
+            <div onClick={addCartItem}><ShopItem image="https://source.unsplash.com/200x200/?jumper" description="Jacket 1" price="60" sid="12"/></div>,
+            <div onClick={addCartItem}><ShopItem image="https://source.unsplash.com/200x200/?jacket" description="Jacket 2" price="40" sid="13"/></div>,
+            <div onClick={addCartItem}><ShopItem image="https://source.unsplash.com/200x200/?hoodie" description="Jacket 3" price="30" sid="14"/></div>
   ]);
 
+  function addCartItem(e) {
+    alert(e.target.id);
+    
+  }
+
   useEffect(() => {
-    function addCartItem(shopItem) {
-      let empty = [];
-      setCartItems(empty => cartItems);
-    }
+
   })
 
   return (
--- src/components/ShopItem.js
+++ src/components/ShopItem.js
@@ -1,12 +1,12 @@
 import '../styles/shopitem.css';
 
-const ShopItem = ({image, description, price}) => {
+const ShopItem = ({image, description, price, sid}) => {
     return (
         <div className="shopItem">
             <img className="itemImage" src={image} />
             <div className="description">{description}</div>
             <div className="price">${price}</div>
-            <div className="add">Add to Cart</div>
+            <div className="add" id={sid}>Add to Cart</div>
         </div>
     )
 }
