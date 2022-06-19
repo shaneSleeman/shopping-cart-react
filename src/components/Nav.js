3ca27be47e635cd420102dc9a78d54d19278b3ec
shaneSleeman <shanesleeman1@gmail.com>
2022-06-19 12:05:05 +1000

    Fix links

--- src/components/Nav.js
+++ src/components/Nav.js
@@ -5,7 +5,7 @@ import cartIcon from '../assets/cart.svg';
 const Nav = ({cartCount}) => {
     return (
         <div className="nav">
-            <div className="navTitle">Shopping Cart</div>
+            <Link className="links" to="/"><div className="navTitle">Shopping Cart</div></Link>
             <div className="navLinks">
                 <div className="navLinks1">
                     <Link className="links" to="/">Home</Link>
@@ -13,8 +13,8 @@ const Nav = ({cartCount}) => {
                 </div>
                 <div className="navLinks2">
                     <Link className="links" to="/cartpage">Cart</Link>
-                    <img className="cartIcon" src={cartIcon} />
-                    <div className="cartItems">{cartCount}</div>
+                    <Link className="links" to="/cartpage"><img className="cartIcon" src={cartIcon} /></Link>
+                    <Link className="links" to="/cartpage"><div className="cartItems">{cartCount}</div></Link>
                 </div>
             </div>
             
